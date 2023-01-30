72d74b2d679577015d78d39ff8d369834bfea444
Patto <nabhatra@yahoo.com>
2023-01-30 20:39:32 +0700

    Initial Commit

---
+++ Netflix Clone/Models/YoutubeSearchResponse.swift
@@ -0,0 +1,23 @@
+//
+//  YoutubeSearchResponse.swift
+//  Netflix Clone
+
+
+import Foundation
+
+
+
+struct YoutubeSearchResponse: Codable {
+    let items: [VideoElement]
+}
+
+
+struct VideoElement: Codable {
+    let id: IdVideoElement
+}
+
+
+struct IdVideoElement: Codable {
+    let kind: String
+    let videoId: String
+}
---
+++ Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
